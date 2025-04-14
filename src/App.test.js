4888cc088f4a10ed97c61a64b906b0383f9cc042
Chidiebere-Nwigwe <chidonnwigz@gmail.com>
2025-04-14 09:37:29 -0600

    created build and test stage

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,25 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Anthony Odinukwe/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Chidiebere Nwigwe/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Chidumebi Obioha/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Gary Simwawa/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
